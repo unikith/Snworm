6b98407010fd1648960cf7874ff98d8f52b33413
unikith <james@thewholefamily.org>
2017-04-30 16:00:25 -0700

    Add project files.

---
+++ Snworm/main.cpp
@@ -0,0 +1,30 @@
+/*
+*	Programmer : James Nelson
+*	Description : a two-player snake game with tank controls and serverl different modes of
+*		play. Uses SFML
+*	Modes : two-player vs, two-player coop, single player 
+*			**coop and single player have leader boards**
+*	4/30/17 - Set up project
+*/
+#include <SFML\Graphics.hpp>
+
+int main(void)
+{
+	sf::RenderWindow window(sf::VideoMode(600, 600), "Snworm");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+		window.clear();
+		window.display();
+	}
+
+	return 0;
+}
